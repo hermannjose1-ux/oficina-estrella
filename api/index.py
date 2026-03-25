a42fe36c70b2e64576d104dbd33019ab7ca8f2bb
hermannjose1-ux <hermannjose1@gmail.com>
2026-03-25 18:05:25 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,14 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.add(ft.Text("⭐ OFICINA ESTRELLA: CONEXIÓN EXITOSA"))
+    page.theme_mode = ft.ThemeMode.DARK
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    
+    page.add(
+        ft.Icon(name=ft.icons.STAR, color="yellow", size=50),
+        ft.Text("OFICINA ESTRELLA", size=30, weight="bold"),
+        ft.Text("¡Conexión establecida con éxito!", color="green")
+    )
 
 app = ft.app(target=main, export_asgi=True)
