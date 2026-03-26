664f2abc5e2ab57833de9865556e6af5ea8d64fe
hermannjose1-ux <hermannjose1@gmail.com>
2026-03-26 00:33:43 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,9 +6,9 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     
     page.add(
-        ft.Icon(name=ft.icons.STAR, color="yellow", size=50),
-        ft.Text("OFICINA ESTRELLA", size=30, weight="bold"),
-        ft.Text("¡Conexión establecida con éxito!", color="green")
+        ft.Icon(name=ft.icons.CHECK_CIRCLE, color="green", size=50),
+        ft.Text("SISTEMA ONLINE", size=30, weight="bold"),
+        ft.Text("Vercel y Flet están comunicados.", color="white70")
     )
 
 app = ft.app(target=main, export_asgi=True)
