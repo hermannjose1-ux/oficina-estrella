bbb1c2743d8350dcb65e25731f254423ace65f1e
hermannjose1-ux <hermannjose1@gmail.com>
2026-03-26 00:43:57 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 import flet as ft
+import flet_fastapi
 
 def main(page: ft.Page):
     page.theme_mode = ft.ThemeMode.DARK
@@ -8,7 +9,8 @@ def main(page: ft.Page):
     page.add(
         ft.Icon(name=ft.icons.CHECK_CIRCLE, color="green", size=50),
         ft.Text("SISTEMA ONLINE", size=30, weight="bold"),
-        ft.Text("Vercel y Flet están comunicados.", color="white70")
+        ft.Text("Flet y Vercel están hablando el mismo idioma.", color="white70")
     )
 
-app = ft.app(target=main, export_asgi=True)
+# ESTO ES LO QUE REEMPLAZA AL ERROR:
+app = flet_fastapi.app(main)
