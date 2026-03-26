6caea3417cf5be201142f389057b35bf258fc4dd
hermannjose1-ux <hermannjose1@gmail.com>
2026-03-26 01:43:13 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,16 +5,20 @@ import fastapi
 app = fastapi.FastAPI()
 
 def main(page: ft.Page):
+    page.title = "Oficina Estrella"
     page.theme_mode = ft.ThemeMode.DARK
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     
     page.add(
-        ft.Icon(name=ft.icons.TELEVISION_OFF_OUTLINED, color="red", size=50),
-        ft.Text("PRUEBA DE CONEXIÓN", size=30, weight="bold"),
-        ft.Text("Si ves esto, matamos al 500 y al Gris.", color="white70")
+        ft.Icon(name=ft.icons.DASHBOARD_CUSTOMIZE_ROUNDED, color="cyan", size=60),
+        ft.Text("SISTEMA OPERATIVO", size=30, weight="bold"),
+        ft.Text("Conexión segura vía HTTP", color="green"),
+        ft.Divider(),
+        ft.ElevatedButton("INICIAR SESIÓN", on_click=lambda _: print("Acceso"))
     )
     page.update()
 
-# Esta es la forma más estable de montar Flet en Vercel
-app.mount("/", flet_fastapi.app(main))
+# ESTA ES LA LÍNEA QUE MATA EL ERROR DE WEBSOCKET:
+# 'web_socket_path=None' obliga a usar Long Polling
+app.mount("/", flet_fastapi.app(main, web_socket_path=None))
